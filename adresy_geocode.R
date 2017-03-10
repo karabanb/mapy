94bc94186501690f1c85136c520a4ea0f173f902
Bartlomiej Karaban <bk@Ula-i-Bartek.local>
2017-03-10 21:13:15 +0100

    modyfikacja popupow

--- adresy_geocode.R
+++ adresy_geocode.R
@@ -1,16 +1,33 @@
 
+### loading required packages ######
 library(ggmap)
 library(leaflet)
+library(dplyr)
+
+#### reading data, checking geocodes ######
+
 adresy<-na.omit(read.table("adresy.csv",sep=";",header = TRUE, stringsAsFactors = FALSE))
 a<-paste(adresy$Numer, adresy$Ulica, adresy$miasto,adresy$Kod.pocztowy,adresy$Kraj, sep = ", ")
-a<-address_cleaner(a)
 b<-geocode(a)
-c<-cbind(a,b)
 d<-cbind(adresy,b)
 
-(m_markers<- leaflet(data = c)%>%
+### creating groups for layers #####
+
+layer_Jerzy<- d%>%
+  filter(.,Pracownik=="Jerzy")
+
+layer_Barbara<-d%>%
+  filter(.,Pracownik=="Barbara")
+
+### generating map ###############
+
+(m_markers<- leaflet()%>%
   addTiles()%>%
-  addMarkers(~lon, ~lat))
+  addProviderTiles(providers$OpenTopoMap)%>%
+  addMarkers(data=layer_Jerzy, ~lon, ~lat, group="Jerzy",label = ~Pracownik)%>%
+  addMarkers(data=layer_Barbara, ~lon, ~lat, group="Barbara",label = ~Pracownik))%>%
+  addLayersControl(overlayGroups=c("Jerzy","Barbara"))
+
 
 write.csv(d,"adresy_wspolrzedne.csv")
 
